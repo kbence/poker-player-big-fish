c45bf32c00921d66233d7d667986a04ea9536e7c
Bence Kiglics <bence.kiglics@gmail.com>
2016-01-30 10:43:48 +0100

    Checking for pair

--- ranking.js
+++ ranking.js
@@ -1,4 +1,12 @@
 CARD_RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a'];
+VALUES = {
+    pair: 20,
+    three: 35
+};
+
+function getPairValue(cards) {
+    return cards[0]['rank'] == card[1]['rank'] ? VALUES.pair : 0;
+}
 
 function getHandValue(cards) {
     var value = 0
@@ -7,6 +15,8 @@ function getHandValue(cards) {
         var rank = CARD_RANKS.indexOf(card['rank'].toLowerCase()) + 2;
         value += rank;
     });
+
+    value += getPairValue(cards);
 
     return value;
 }
