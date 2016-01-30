965324aa9161a818aa394e26d24512d76451aebb
Bence Kiglics <bence.kiglics@gmail.com>
2016-01-30 11:16:20 +0100

    Refactored tests

--- test/ranking_test.js
+++ test/ranking_test.js
@@ -1,26 +1,32 @@
 var assert = require('assert');
 var ranking = require('../ranking');
 
+var CARD_SUITS = {'h': 'hearts', 'c': 'clubs', 's': 'spades', 'd': 'diamonds'};
+
+function createCard(code) {
+    var suit = CARD_SUITS[code.charAt(0)];
+    var rank = code.substr(1);
+
+    return {rank:rank, suit:suit};
+}
+
 describe('numberOfPairs', function() {
-    it('should return 0 for no cards', function() {
-        var cards = [{rank: '5'}, {rank: '6'}];
-        assert.equal(ranking.numberOfPairs(cards), 0);
-    });
+    var CASES = [
+        { input: ['H5', 'H6'], output: 0 },
+        { input: ['C6', 'H6'], output: 1 },
+        { input: ['C6', 'H6', 'C5', 'D7', 'H7'], output: 2 },
+        { input: ['C6', 'H6', 'C5', 'D5', 'D7', 'H7'], output: 2 },
+    ];
 
-    it('should return 1 if it has a pair', function() {
-        var cards = [{rank: '6'}, {rank: '6'}];
-        assert.equal(ranking.numberOfPairs(cards), 1);
-    });
-
-    it('should return 2 if it has two pairs', function() {
-        var cards = [{rank: '6'}, {rank: '6'}, {rank: '5'}, {rank: '7'}, {rank: '7'}];
-        assert.equal(ranking.numberOfPairs(cards), 2);
-    });
-
-    it('should return 2 even if it has more pairs', function() {
-        var cards = [{rank: '6'}, {rank: '6'}, {rank: '5'}, {rank: '5'}, {rank: '7'}, {rank: '7'}];
-        assert.equal(ranking.numberOfPairs(cards), 2);
-    });
+    for (var c = 0; c < CASES.length; c++) {
+        (function(Case) {
+            it('should return ' + Case.output + ' for ' + Case.input.join(', '), function() {
+                var input = Case.input;
+                input = Case.input.map(createCard);
+                assert.equal(Case.output, ranking.numberOfPairs(input))
+            })
+        })(CASES[c]);
+    }
 });
 
 describe('getHandValue', function() {
