65a8b410628ca6c32a543d2f6bef7f551fdc6188
Bence Berenyi <bence@ujweblap.com>
2016-01-30 10:50:36 +0100

    rank

--- player.js
+++ player.js
@@ -1,10 +1,23 @@
+var Player_actions = require("./player_actions");
+var ranking = require("./ranking.js");
+var minimumToCall = 10;
+var minimumToRaise = 20;
 
 module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(10000);
+    var action = new Player_actions(game_state);
+    var handRank = ranking.getHandValue(game_state.players[game_state.in_action]["hole_cards"]);
+
+    if (handRank < minimumToCall) {
+      bet(action.doFold());
+    } else if (handRank >= minimumToCall && handRank < minimumToRaise) {
+      bet(action.doCall());
+    } else {
+      bet(action.doAllIn());
+    }
   },
 
   showdown: function(game_state) {
